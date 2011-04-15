98904eb42f58bf489872a800eafa360601e68b2c
Jarrod Taylor <jarrodtaylor@me.com>
2011-04-15 04:15:19 -0400

    added User class

--- libraries/user.rb
+++ libraries/user.rb
@@ -1,3 +1,23 @@
 get '/profile/?' do
   erb :profile
 end
+
+
+class User
+  include DataMapper::Resource
+  
+  property    :id,          Serial
+  property    :deleted_at,  ParanoidDateTime
+  timestamps  :at
+  
+  property :email,      String
+  property :password,   String
+  property :mobile,     String
+  property :age,        String
+  property :gender,     String, :default => 'unknown'
+  property :zip,        String
+  
+  property :optin,      Boolean, :default => false
+  property :optin_msg,  String
+  
+end
