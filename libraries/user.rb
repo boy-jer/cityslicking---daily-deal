5d4ec385bfb8bfcc6180592b5140be1d513baed7
Jarrod Taylor <jarrodtaylor@me.com>
2011-04-15 12:27:00 -0400

    added account detail saving to /profile

--- libraries/user.rb
+++ libraries/user.rb
@@ -23,7 +23,43 @@ get '/sign-out/?' do
 end
 
 get '/profile/?' do
+  @user = User.get(session[:user])
   erb :profile
+end
+
+post '/profile/?' do
+  params[:email].strip!
+  params[:email].downcase!
+  params[:password].strip!
+  params[:password].downcase!
+  params[:zip].strip!
+  params[:age].strip!
+  
+  if params[:email].length == 0 || params[:password].length == 0
+    session[:flash] = 'You must provide both and email and a password to update.'
+    redirect '/profile'
+  end
+  
+  user = User.get(session[:user])
+  
+  unless params[:email] == user.email
+    if User.all(:email => params[:email]).count > 0
+      session[:flash] = 'Email address is already in use. Try again.'
+      redirect '/profile'
+    end
+  end
+  
+  params[:gender] = 'unknown' unless params[:gender]
+  
+  user.update(
+    :email    => params[:email],
+    :password => params[:password],
+    :zip      => params[:zip],
+    :age      => params[:age],
+    :gender   => params[:gender]
+  )
+  session[:flash] = 'Your account details have been updated.'
+  redirect '/profile'
 end
 
 
