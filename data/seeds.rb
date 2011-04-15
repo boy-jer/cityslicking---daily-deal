99bce267dc98b3220814d8f1f7a46b0aa95626ec
Jarrod Taylor <jarrodtaylor@me.com>
2011-04-15 11:45:09 -0400

    added seed users

--- data/seeds.rb
+++ data/seeds.rb
@@ -4,3 +4,6 @@ stockton  = City.create(:name => 'Stockton, CA',  :short_name => 'stockton')
 lodi      = City.create(:name => 'Lodi, CA',      :short_name => 'lodi'    )
 elk_grove = City.create(:name => 'Elk Grove, CA', :short_name => 'elkgrove')
 turlock   = City.create(:name => 'Turlock, CA',   :short_name => 'turlock' )
+
+jarrod = User.create(:email => 'jarrodtaylor@cityslicking.com', :password => 'demo')
+john   = User.create(:email => 'john@cityslicking.com',         :password => 'demo')
