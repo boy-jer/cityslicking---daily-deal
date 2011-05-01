54f48f39dd3fad2b135b6b935f7bb0f3cacf1c39
Jarrod Taylor <jarrodtaylor@me.com>
2011-05-01 16:28:05 -0500

    started combining mobile and html view files directories

--- libraries/device_detection.rb
+++ libraries/device_detection.rb
@@ -15,15 +15,23 @@
       ]
       mobile_user_agent_patterns.any? {|r| request.env['HTTP_USER_AGENT'] =~ r}
     end
+    
+    
+  # Returns a view file name with .mobile if it exists, otherwise returns the normal .html file name
+    
+    def mobile_file(file)
+      File.exists?("#{settings.views}/#{file}#{@device}.html") ? view = "#{file}#{@device}" : view = file
+    end
   
   
   # Replaces Sinatra's erb method
   # View path is a string instead of a symbol
-  # To use :locals you must first provide a :layout (find a fix for this)
+  # To use :locals you must first provide a :layout (find a fix for this) [Is this still an issue?]
   
-    def deliver(view, options = {:layout => 'layout', :locals => {}})
-      options[:layout] == false ? layout = false : layout = :"#{@device}/#{options[:layout]}"
-      erb :"#{@device}/#{view}", :layout => layout, :locals => options[:locals]
+    def deliver(view, options = {:layout => 'layout', :locals => {}})      
+      options[:layout] = 'layout' unless options[:layout] || options[:layout] == false
+      options[:layout] == false ? layout = false : layout = mobile_file(options[:layout]).to_sym
+      erb mobile_file(view).to_sym, :layout => layout, :locals => options[:locals]
     end
   
   
@@ -33,5 +41,5 @@
 # Sets @device based on mobile_request? (see above)
   
   before do
-    mobile_request? ? @device = 'mobile' : @device = 'desktop'
+    mobile_request? ? @device = '.mobile' : @device = ''
   end
