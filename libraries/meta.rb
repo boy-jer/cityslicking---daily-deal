a2966fdb071f3e3409ac166e216567e4edb3e5a9
Jarrod Taylor <jarrodtaylor@me.com>
2011-05-02 05:16:17 -0500

    added merchant agreement online form

--- libraries/meta.rb
+++ libraries/meta.rb
@@ -23,3 +23,14 @@ get '/admin/resources/advertising-and-promotions-agreement/?' do
   auth_admin
   deliver 'admin/resources/advertising-and-promotions-agreement'
 end
+
+post '/admin/resources/advertising-and-promotions-agreement/?' do
+  auth_admin
+  Pony.mail(:via => :smtp, :via_options => settings.mail_server,
+    :to        => 'jarrodtaylor@me.com',
+    :subject   => 'CitySlicking Merchant Agreement',
+    :html_body => erb(:'admin/resources/advertising-and-promotions-agreement_email', :layout => false)
+  )
+  session[:flash] = 'Check your inbox.'
+  redirect '/admin/resources'
+end
