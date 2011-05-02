5a5b832b41e2fb545558d20fad11c37a68fcf494
Jarrod Taylor <jarrodtaylor@me.com>
2011-05-02 00:25:41 -0500

    fixed up home page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -19,46 +19,94 @@ jQuery(function()
 		return false;
 	});
 	
-	function toggle_feature(feature)
+	function featured_deal(position)
 	{
-		if ( $('#feature-1').is(":visible") ) {var current = 1;}
-		if ( $('#feature-2').is(":visible") ) {var current = 2;}
-		if ( $('#feature-3').is(":visible") ) {var current = 3;}
-		if (!(current == feature))
+		if (position == 1)
 		{
-			if (current > feature)
-			{
-				var to   = 'left';
-				var away = 'right';
-			}
-			else
-			{
-				var to   = 'right';
-				var away = 'left';
-			}
-			if ( $('#feature-1').is(":visible") ) { $('#feature-1').hide('fade'); $('#feature-1 .deal-info').show().hide('slide', {direction: away}); }
-			if ( $('#feature-2').is(":visible") ) { $('#feature-2').hide('fade'); $('#feature-2 .deal-info').show().hide('slide', {direction: away}); }
-			if ( $('#feature-3').is(":visible") ) { $('#feature-3').hide('fade'); $('#feature-3 .deal-info').show().hide('slide', {direction: away}); }
-			$('#feature-' + feature).show('fade');
-			$('#feature-' + feature + ' .deal-info').show('slide', {direction: to});
+		  $("#deal-1").animate({left: "7px"}, 300 );
+			$("#deal-2").animate({left: "787px"	}, 300 );
+			$("#deal-3").animate({left: "1567px"	}, 300 );
+			$("#button-1, #button-2, #button-3").removeClass("active inactive").addClass("inactive");
+			$("#button-1").removeClass("inactive").addClass("active");
+			$("#left-arrow").attr("href", "3");
+			$("#right-arrow").attr("href", "2");
+		}
+		else if (position == 2)
+		{
+		  $("#deal-1").animate({left: "-773px"}, 300 );
+			$("#deal-2").animate({left: "7px"	}, 300 );
+			$("#deal-3").animate({left: "787px"	}, 300 );
+			$("#button-1, #button-2, #button-3").removeClass("active inactive").addClass("inactive");
+			$("#button-2").removeClass("inactive").addClass("active");
+			$("#left-arrow").attr("href", "1");
+			$("#right-arrow").attr("href", "3");
+		}
+		else if (position == 3)
+		{
+		  $("#deal-1").animate({left: "-1553px"}, 300 );
+			$("#deal-2").animate({left: "-773px"	}, 300 );
+			$("#deal-3").animate({left: "7px"	}, 300 );
+			$("#button-1, #button-2, #button-3").removeClass("active inactive").addClass("inactive");
+			$("#button-3").removeClass("inactive").addClass("active");
+			$("#left-arrow").attr("href", "2");
+			$("#right-arrow").attr("href", "1");
 		}
 	}
+			
+	$('#button-1').click(function()
+	{
+		featured_deal(1);
+		return false;
+	});
+
+	$('#button-2').click(function()
+	{
+		featured_deal(2);
+		return false;
+	});
+
+	$('#button-3').click(function()
+	{
+		featured_deal(3);
+		return false;
+	});
 	
-	$('.button-1, .arrow-1').click(function()
+	$("#left-arrow").click(function()
 	{
-		toggle_feature(1);
+		featured_deal($("#left-arrow").attr("href"));
 		return false;
 	});
 
-	$('.button-2, .arrow-2').click(function()
+	$("#right-arrow").click(function()
 	{
-		toggle_feature(2);
+		featured_deal($("#right-arrow").attr("href"));
+		return false;
+	});
+	
+	$("#mobile-btn-1").click(function()
+	{
+		$("#feature-1, #feature-2, #feature-3").hide();
+		$("#feature-1").show();
+		$("#mobile-btn-1, #mobile-btn-2, #mobile-btn-3").removeClass("active inactive").addClass("inactive");
+		$("#mobile-btn-1").removeClass("inactive").addClass("active");
 		return false;
 	});
 
-	$('.button-3, .arrow-3').click(function()
+	$("#mobile-btn-2").click(function()
 	{
-		toggle_feature(3);
+		$("#feature-1, #feature-2, #feature-3").hide();
+		$("#feature-2").show();
+		$("#mobile-btn-1, #mobile-btn-2, #mobile-btn-3").removeClass("active inactive").addClass("inactive");
+		$("#mobile-btn-2").removeClass("inactive").addClass("active");
+		return false;
+	});
+
+	$("#mobile-btn-3").click(function()
+	{
+		$("#feature-1, #feature-2, #feature-3").hide();
+		$("#feature-3").show();
+		$("#mobile-btn-1, #mobile-btn-2, #mobile-btn-3").removeClass("active inactive").addClass("inactive");
+		$("#mobile-btn-3").removeClass("inactive").addClass("active");
 		return false;
 	});
 	
@@ -71,6 +119,7 @@ jQuery(function()
 	$('#current-city').click(function()
 	{
 		$('#other-cities').toggle("blind");
+		return false;
 	});
 	
 	if ($('#finish-verification').length)
