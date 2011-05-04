f51f03caed4a13a3218a46a75d5bb9572dbff5d6
Jarrod Taylor <jarrodtaylor@me.com>
2011-05-03 22:32:58 -0400

    created a library for /resources

--- libraries/meta.rb
+++ libraries/meta.rb
@@ -13,26 +13,3 @@ end
 get '/policies/?' do
   deliver 'policies'
 end
-
-get '/admin/resources/?' do
-  auth_admin
-  deliver 'admin/resources/resources'
-end
-
-get '/admin/resources/advertising-and-promotions-agreement/?' do
-  auth_admin
-  deliver 'admin/resources/advertising-and-promotions-agreement'
-end
-
-post '/admin/resources/advertising-and-promotions-agreement/?' do
-  auth_admin
-  Pony.mail(:via => :smtp, :via_options => settings.mail_server,
-    :to        => 'johndickey@city-slicking.com',
-    :cc        => 'mattcross@city-slicking.com',
-    :bcc       => 'jarrodtaylor@city-slicking.com',
-    :subject   => 'CitySlicking Merchant Agreement',
-    :html_body => erb(:'admin/resources/advertising-and-promotions-agreement_email', :layout => false)
-  )
-  session[:flash] = 'Check your inbox.'
-  redirect '/admin/resources'
-end
