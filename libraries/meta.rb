0cf8851549a8658452ce60023effee7863d9045c
Jarrod Taylor <jarrodtaylor@me.com>
2011-05-08 21:06:09 -0400

    added Contact Us page

--- libraries/meta.rb
+++ libraries/meta.rb
@@ -13,3 +13,17 @@ end
 get '/policies/?' do
   deliver 'policies'
 end
+
+get '/contact-us/?' do
+  deliver 'contact'
+end
+
+post '/contact-us/?' do
+  Pony.mail(:via => :smtp, :via_options => settings.mail_server,
+    :to => 'johndickey@city-slicking.com',
+    :subject => 'Contact Form: ' + params[:subject],
+    :body => params[:body]
+  )
+  session[:flash] = "Thanks, we'll be in touch."
+  redirect '/contact-us'
+end
