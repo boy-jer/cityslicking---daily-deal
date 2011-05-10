8cedc3015feebc78fb6563a4d5df28563a88cb53
Jarrod Taylor <jarrodtaylor@me.com>
2011-05-10 00:08:48 -0400

    set up cron job psuedo code

---
+++ libraries/cron.rb
@@ -0,0 +1,19 @@
+class Cronjob
+  
+  def daily_deals
+    "Here's your Slick deal of the day.  Remember every deal you opt for is fun for you, good for your friendly merchant and even helps out your community!  You'll find many more at City-Slicking.com."
+  end
+  
+  def thank_you
+    "It looks like you scored a deal with (merchant) and we hope it's a great experience!  On their behalf, we thank you.  As a matter of fact, here's a special discount to encourage you to come back again."
+  end
+  
+  def super_slicker
+    "It looks like you scored a deal with (merchant) and we hope it's a great experience!  On their behalf, we thank you.  As a matter of fact, here's a special opportunity - if you will share the good news with 50 of your friends and they use this referral code, (code), when they register and/or opt for this deal, (merchant) will give you a free (free item, meal, etc) for your kind support."
+  end
+  
+  def reminder
+    "Hi, just a friendly reminder that your CitySlicking deal will be going live tonight at midnight.  Just let us know if you've got any ??s. We want your campaign to be a great success."
+  end
+  
+end
