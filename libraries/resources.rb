2cef965d26ae8c323bac23594f76c67ecff99d00
Jarrod Taylor <jarrodtaylor@me.com>
2011-05-27 04:01:22 -0400

    added blogger agreement

--- libraries/resources.rb
+++ libraries/resources.rb
@@ -3,6 +3,22 @@ get '/admin/resources/?' do
   deliver 'admin/resources/resources'
 end
 
+get '/admin/resources/blogger-agreement/?' do
+  auth_admin
+  deliver 'admin/resources/blogger-agreement'
+end
+
+post '/admin/resources/blogger-agreement/?' do
+  auth_admin
+  Pony.mail(:via => :smtp, :via_options => settings.mail_server,
+    :to        => 'johndickey@city-slicking.com',
+    :subject   => 'CitySlicking Blogger Agreement',
+    :html_body => erb(:'admin/resources/blogger-agreement_email', :layout => false)
+  )
+  session[:flash] = 'Check your inbox.'
+  redirect '/admin/resources'
+end
+
 get '/admin/resources/advertising-and-promotions-agreement/?' do
   auth_admin
   deliver 'admin/resources/advertising-and-promotions-agreement'
