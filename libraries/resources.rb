a128b10e7a9c982d02c1899e614879b01c80c2c1
Jarrod Taylor <jarrodtaylor@me.com>
2011-06-06 13:04:45 -0400

    added merchant agreement web pages and emailers

--- libraries/resources.rb
+++ libraries/resources.rb
@@ -24,20 +24,92 @@ post '/admin/resources/blogger-agreement/?' do
   redirect '/admin/resources'
 end
 
-get '/admin/resources/advertising-and-promotions-agreement/?' do
+get '/admin/resources/merchants/10/?' do
   auth_admin
-  deliver 'admin/resources/advertising-and-promotions-agreement'
+  deliver 'admin/resources/merchants/10'
 end
 
-post '/admin/resources/advertising-and-promotions-agreement/?' do
+post '/admin/resources/merchants/10/?' do
   auth_admin
   Pony.mail(:via => :smtp, :via_options => settings.mail_server,
     :to        => 'johndickey@city-slicking.com',
     :cc        => 'mattcross@city-slicking.com',
     :bcc       => 'jarrodtaylor@city-slicking.com',
     :subject   => 'CitySlicking Merchant Agreement',
-    :html_body => erb(:'admin/resources/advertising-and-promotions-agreement_email', :layout => false)
+    :html_body => erb(:'admin/resources/merchants/10_email', :layout => false)
   )
-  session[:flash] = 'Check your inbox.'
+  session[:flash] = 'Merchant agreement sent.'
+  redirect '/admin/resources'
+end
+
+get '/admin/resources/merchants/100/?' do
+  auth_admin
+  deliver 'admin/resources/merchants/100'
+end
+
+post '/admin/resources/merchants/100/?' do
+  auth_admin
+  Pony.mail(:via => :smtp, :via_options => settings.mail_server,
+    :to        => 'johndickey@city-slicking.com',
+    :cc        => 'mattcross@city-slicking.com',
+    :bcc       => 'jarrodtaylor@city-slicking.com',
+    :subject   => 'CitySlicking Merchant Agreement',
+    :html_body => erb(:'admin/resources/merchants/100_email', :layout => false)
+  )
+  session[:flash] = 'Merchant agreement sent.'
+  redirect '/admin/resources'
+end
+
+get '/admin/resources/merchants/200/?' do
+  auth_admin
+  deliver 'admin/resources/merchants/200'
+end
+
+post '/admin/resources/merchants/200/?' do
+  auth_admin
+  Pony.mail(:via => :smtp, :via_options => settings.mail_server,
+    :to        => 'johndickey@city-slicking.com',
+    :cc        => 'mattcross@city-slicking.com',
+    :bcc       => 'jarrodtaylor@city-slicking.com',
+    :subject   => 'CitySlicking Merchant Agreement',
+    :html_body => erb(:'admin/resources/merchants/200_email', :layout => false)
+  )
+  session[:flash] = 'Merchant agreement sent.'
+  redirect '/admin/resources'
+end
+
+get '/admin/resources/merchants/300/?' do
+  auth_admin
+  deliver 'admin/resources/merchants/300'
+end
+
+post '/admin/resources/merchants/300/?' do
+  auth_admin
+  Pony.mail(:via => :smtp, :via_options => settings.mail_server,
+    :to        => 'johndickey@city-slicking.com',
+    :cc        => 'mattcross@city-slicking.com',
+    :bcc       => 'jarrodtaylor@city-slicking.com',
+    :subject   => 'CitySlicking Merchant Agreement',
+    :html_body => erb(:'admin/resources/merchants/300_email', :layout => false)
+  )
+  session[:flash] = 'Merchant agreement sent.'
+  redirect '/admin/resources'
+end
+
+get '/admin/resources/merchants/free/?' do
+  auth_admin
+  deliver 'admin/resources/merchants/free'
+end
+
+post '/admin/resources/merchants/free/?' do
+  auth_admin
+  Pony.mail(:via => :smtp, :via_options => settings.mail_server,
+    :to        => 'johndickey@city-slicking.com',
+    :cc        => 'mattcross@city-slicking.com',
+    :bcc       => 'jarrodtaylor@city-slicking.com',
+    :subject   => 'CitySlicking Merchant Agreement',
+    :html_body => erb(:'admin/resources/merchants/free_email', :layout => false)
+  )
+  session[:flash] = 'Merchant agreement sent.'
   redirect '/admin/resources'
 end
